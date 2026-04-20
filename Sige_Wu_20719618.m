e7835689a5e4578485a882cb07489f36374a14dc
Sige Wu <ssysw20@nottingham.edu.cn>
2026-04-21 03:01:58 +0800

    Preliminary task complete, UNO development board successfully connected, LED can now blink.

--- Sige_Wu_20719618.m
+++ Sige_Wu_20719618.m
@@ -3,8 +3,23 @@
 
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
+clear;
 
-% Insert answers here
+% Connect the UNO arduino device through the COM8 port
+a = arduino("COM8","Uno");
+
+% LED blink loop
+while true
+    % Turn on the LED connected to digital channel 2
+    writeDigitalPin(a,'D2',1);
+    % Wait for 0.5 seconds
+    pause(0.5);
+    % Turn off the LED connected to digital channel 2
+    writeDigitalPin(a,'D2',0);
+    % Wait for 0.5 seconds
+    pause(0.5);
+end
+
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
